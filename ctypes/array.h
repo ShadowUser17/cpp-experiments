addcdf893e47b7677469522920ee6da9f4d9a86a
ShadowUser17 <linuxforce17@gmail.com>
2021-12-28 22:51:04 +0200

    Changes to be committed: 	modified:   ctypes/array.h (Updated and fixed.)

--- ctypes/array.h
+++ ctypes/array.h
@@ -1,10 +1,8 @@
 #ifndef __ARRAY__
 #define __ARRAY__
 
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <errno.h>
 
 
 struct Array {
@@ -19,7 +17,7 @@ int array_len(struct Array* ptr) {
         return ptr->length;
     }
 
-    return 0;
+    return -1;
 }
 
 
@@ -28,13 +26,15 @@ int array_cap(struct Array* ptr) {
         return ptr->capacity;
     }
 
-    return 0;
+    return -1;
 }
 
 
 void* array_index(struct Array* ptr, size_t type, int pos) {
     if(ptr != NULL) {
-        return ptr->items + (type * pos);
+        if((pos > -1) && (pos < ptr->capacity)) {
+            return (ptr->items + (type * pos));
+        }
     }
 
     return NULL;
@@ -44,33 +44,86 @@ void* array_index(struct Array* ptr, size_t type, int pos) {
 struct Array* array_init(size_t type, int cap) {
     struct Array* array = (struct Array*)malloc(sizeof(struct Array));
 
-    if(array == NULL) {
-        fprintf(stderr, "%s\n", strerror(errno));
-        return NULL;
-    }
+    if(array == NULL) return NULL;
 
-    if(cap <= 1) { cap = 10; }
+    array->items = NULL;
+    array->length = 0;
+    array->capacity = 0;
+
+    if(cap < 10) cap = 10;
 
     array->items = calloc(cap, type);
 
-    if(array->items == NULL) {
-        fprintf(stderr, "%s\n", strerror(errno));
-        return NULL;
-    }
+    if(array->items == NULL) return NULL;
 
-    array->length = 0;
     array->capacity = cap;
 
-    memset(array->items, 0, (type * array->capacity));
+    memset(array->items, 0, (type * cap));
     return array;
 }
 
 
 void array_free(struct Array* ptr) {
     if(ptr != NULL) {
-        free(ptr->items);
+        if(ptr->items != NULL) free(ptr->items);
+
         free(ptr);
     }
+}
+
+
+int array_copy(struct Array* dst, struct Array* src, size_t type) {
+    if((dst != NULL) && (src != NULL)) {
+        void* dst_index = NULL;
+        void* src_index = NULL;
+        int it = 0;
+
+        while(it < src->length) {
+            dst_index = array_index(dst, type, it);
+            src_index = array_index(src, type, it);
+
+            if((dst_index != NULL) && (src_index != NULL)) {
+                memcpy(dst_index, src_index, type);
+                dst->length += 1;
+                it += 1;
+
+            } else break;
+        }
+
+        return it;
+    }
+
+    return -1;
+}
+
+
+struct Array* array_append(struct Array* ptr, void* item, size_t type) {
+    if(ptr != NULL) {
+        if(ptr->length < ptr->capacity) {
+            memcpy(array_index(ptr, type, ptr->length), item, type);
+            ptr->length += 1;
+            return ptr;
+
+        } else {
+            struct Array* new_ptr = array_init(type, (ptr->capacity + 10));
+
+            if(new_ptr != NULL) {
+                if(array_copy(new_ptr, ptr, type) > 0) {
+                    array_free(ptr);
+
+                    memcpy(array_index(new_ptr, type, new_ptr->length), item, type);
+                    new_ptr->length += 1;
+                    return new_ptr;
+
+                } else {
+                    array_free(new_ptr);
+                    return ptr;
+                }
+            }
+        }
+    }
+
+    return NULL;
 }
 
 #endif // __ARRAY__
