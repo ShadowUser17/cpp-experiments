58d25b2ce2488f8c3f44f3012523f0bd98d93647
ShadowUser17 <linuxforce17@gmail.com>
2021-12-31 22:15:18 +0200

    Changes to be committed: 	modified:   ctypes/cstring.h

--- ctypes/cstring.h
+++ ctypes/cstring.h
@@ -10,4 +10,30 @@ struct String {
     int   length;
 };
 
+
+struct String* string_init(void) {
+    struct String* string = (struct String*)malloc(sizeof(struct String));
+    if(!string) return NULL;
+
+    string->items = (char*)calloc(16, sizeof(char));
+    if(!string->items) {
+        free(string);
+        return NULL;
+    }
+
+    string->items[15] = '\0';
+    string->length = 15;
+
+    return string;
+}
+
+
+void string_free(struct String* ptr) {
+    if(!ptr) {
+        if(ptr->items) free(ptr->items);
+
+        free(ptr);
+    }
+}
+
 #endif // __CSTRING_H__
