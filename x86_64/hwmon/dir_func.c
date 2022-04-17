e7ac806331f45538d7d0e1c7e757e428bb77f122
ShadowUser17 <linuxforce17@gmail.com>
2022-04-17 14:32:53 +0300

    Changes to be committed: 	modified:   .gitignore 	renamed:    ATmega328P/README.md -> aarch64/README.md 	renamed:    Attiny85/README.md -> avr/atmega328p/README.md 	renamed:    README.md -> avr/attiny85/README.md 	renamed:    chmac/README.md -> noarch/README.md 	renamed:    buffer_overflow.c -> noarch/buffer_overflow.c 	renamed:    type_size.c -> noarch/type_size.c 	renamed:    ctypes/README.md -> x86_64/chmac/README.md 	renamed:    hwmon/README.md -> x86_64/ctypes/README.md 	renamed:    ctypes/carray.c -> x86_64/ctypes/carray.c 	renamed:    ctypes/carray.h -> x86_64/ctypes/carray.h 	renamed:    ctypes/cdict.c -> x86_64/ctypes/cdict.c 	renamed:    ctypes/cdict.h -> x86_64/ctypes/cdict.h 	renamed:    ctypes/cstring.c -> x86_64/ctypes/cstring.c 	renamed:    ctypes/cstring.h -> x86_64/ctypes/cstring.h 	new file:   x86_64/hwmon/README.md 	renamed:    hwmon/dir_func.h -> x86_64/hwmon/dir_func.c 	new file:   x86_64/hwmon/dir_func.h 	renamed:    hwmon/main.c -> x86_64/hwmon/main.c

--- x86_64/hwmon/dir_func.c
+++ x86_64/hwmon/dir_func.c
@@ -1,12 +1,8 @@
-#ifndef __DIR_FUNC_H__
-#define __DIR_FUNC_H__
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 #include <dirent.h>
 
+#include "dir_func.h"
 
-
-#endif // __DIR_FUNC_H__
---
+++ x86_64/hwmon/dir_func.h
@@ -0,0 +1,6 @@
+#ifndef __DIR_FUNC_H__
+#define __DIR_FUNC_H__
+
+
+
+#endif // __DIR_FUNC_H__
