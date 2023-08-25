051267ea94e26524f429ed59710aa11c0fc58dd7
ShadowUser17 <linuxforce17@gmail.com>
2023-08-25 10:51:44 +0300

    Changes to be committed: 	renamed:    aarch64/README.md -> README.md 	deleted:    x86_64/chmac/README.md 	deleted:    x86_64/hwmon/README.md 	deleted:    x86_64/hwmon/dir_func.c 	deleted:    x86_64/hwmon/dir_func.h 	deleted:    x86_64/hwmon/main.c

--- x86_64/hwmon/dir_func.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
-#include <dirent.h>
-
-#include "dir_func.h"
-
--- x86_64/hwmon/dir_func.h
+++
@@ -1,6 +0,0 @@
-#ifndef __DIR_FUNC_H__
-#define __DIR_FUNC_H__
-
-
-
-#endif // __DIR_FUNC_H__
